07a0bb123b413d21b1d2e3bc9e17852ecb60a194
hasnaindev23 <hasnainop525@gmail.com>
2024-03-06 15:06:56 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,93 @@
+// This is code of today class
+// let num1: number = 10;
+// let num2: number = 6;
+// let cart: number = num1 + num2 + 10;
+// console.log(cart); //this will return 16
+// let num1:number = 10;
+// let num2:number = 2;
+// const sub:number = num1 - num2;
+// console.log(sub);
+// let num1: number = 10;
+// let num2: number = 2;
+// let mul:number = num1 * num2;
+// console.log("multiply: ", mul);
+// let num1: number = 10;
+// let num2: number = 2;
+// let divide: number = num1 / num2;
+// console.log("divide: ", divide); //this will return 5
+// const pencils: number = 25;
+// const students: number = 5;
+// const mod: number = pencils % students;
+// console.log("mod: ", mod);
+// const num1: number = 5;
+// const num2: number = 2;
+// const power: number = num1 ** num2;
+// console.log("power: ", power);
+//unary operator
+// let a: number = 5;
+// let b: number = ++a;
+// console.log(a, b);
+// const firstName: string = "Ubaid";
+// const lastName: string = "ur Rehman";
+// const fullName: string = firstName + " " + lastName;
+// console.log(fullName);
+// const firstName: string = "Ubaid";
+// const lastName: string = "ur Rehman";
+// console.log(firstName - lastName);
+// let num1: number = 10;
+// let myName: string = "Hasnain age:";
+// console.log(myName + num1);
+// let n = 5;
+// n = n + 5;
+// n += 5;
+// console.log(n);
+/*n = n - 5;
+console.log(n);*/
+//In The Form Of Varables
+//This is an arithmetic operator
+var a = 5;
+var b = 2;
+var add = a + b;
+console.log("addition: ", add);
+var sub = a - b;
+console.log("subtraction:", sub);
+var mul = a * b;
+console.log("multiplication:", mul);
+var div = a / b;
+console.log("division:", div);
+var mod = a % b;
+console.log("modulus:", mod);
+var power = Math.pow(a, b);
+console.log("power:", power);
+//This is an assignment operator
+var x = 5;
+x += 5;
+console.log(x);
+x -= 5;
+console.log(x);
+x *= 5;
+console.log(x);
+x /= 5;
+console.log(x);
+x %= 5;
+console.log(x);
+x = Math.pow(x, 5);
+console.log(x);
+//This is a comparison operator
+var num1 = 5;
+var num2 = 2;
+console.log(num1 == num2);
+console.log(num1 === num2);
+console.log(num1 != num2);
+console.log(num1 > num2);
+console.log(num1 < num2);
+console.log(num1 >= num2);
+console.log(num1 <= num2);
+console.log("--------------------------------");
+//This is a logical operator
+var aValue = 5;
+var bValue = 2;
+console.log(aValue == bValue && aValue > bValue); //this will give me false
+console.log(aValue == bValue || aValue > bValue); //this will give me true
+console.log(!(aValue == bValue)); //this will give me true
+// this is over 4th class of typescript today class we will learn about all TS Operator
---
+++ app.ts
@@ -0,0 +1,142 @@
+// This is code of today class
+
+// let num1: number = 10;
+// let num2: number = 6;
+// let cart: number = num1 + num2 + 10;
+// console.log(cart); //this will return 16
+
+// let num1:number = 10;
+// let num2:number = 2;
+// const sub:number = num1 - num2;
+// console.log(sub);
+
+
+// let num1: number = 10;
+// let num2: number = 2;
+// let mul:number = num1 * num2;
+// console.log("multiply: ", mul);
+
+// let num1: number = 10;
+// let num2: number = 2;
+// let divide: number = num1 / num2;
+// console.log("divide: ", divide); //this will return 5
+
+
+
+// const pencils: number = 25;
+// const students: number = 5;
+// const mod: number = pencils % students;
+// console.log("mod: ", mod);
+
+// const num1: number = 5;
+// const num2: number = 2;
+// const power: number = num1 ** num2;
+// console.log("power: ", power);
+
+
+//unary operator
+
+// let a: number = 5;
+// let b: number = ++a;
+// console.log(a, b);
+
+
+// const firstName: string = "Ubaid";
+// const lastName: string = "ur Rehman";
+// const fullName: string = firstName + " " + lastName;
+// console.log(fullName);
+
+
+// const firstName: string = "Ubaid";
+// const lastName: string = "ur Rehman";
+// console.log(firstName - lastName);
+
+
+// let num1: number = 10;
+// let myName: string = "Hasnain age:";
+// console.log(myName + num1);
+
+
+// let n = 5;
+// n = n + 5;
+// n += 5;
+// console.log(n);
+
+/*n = n - 5;
+console.log(n);*/
+
+
+
+//In The Form Of Varables
+//This is an arithmetic operator
+let a: number = 5;
+let b: number = 2;
+let add = a + b;
+console.log("addition: ", add);
+
+let sub = a - b;
+console.log("subtraction:", sub);
+
+let mul = a * b;
+console.log("multiplication:", mul);
+
+let div = a / b;
+console.log("division:", div);
+
+let mod = a % b;
+console.log("modulus:", mod);
+
+let power = a ** b;
+console.log("power:", power);
+
+
+
+//This is an assignment operator
+let x: number = 5;
+x += 5;
+console.log(x);
+
+x -= 5;
+console.log(x);
+
+x *= 5;
+console.log(x);
+
+x /= 5;
+console.log(x);
+
+x %= 5;
+console.log(x);
+
+x **= 5;
+console.log(x);
+
+
+
+//This is a comparison operator
+let num1: number = 5;
+let num2: number = 2;
+console.log(num1 == num2);
+
+console.log(num1 === num2);
+
+console.log(num1 != num2);
+
+console.log(num1 > num2);
+
+console.log(num1 < num2);
+
+console.log(num1 >= num2);
+
+console.log(num1 <= num2);
+console.log("--------------------------------");
+
+//This is a logical operator
+let aValue: number = 5;
+let bValue: number = 2;
+console.log(aValue == bValue && aValue > bValue); //this will give me false
+console.log(aValue == bValue || aValue > bValue); //this will give me true
+console.log(!(aValue == bValue)) //this will give me true
+
+
+// this is over 4th class of typescript today class we will learn about all TS Operator
